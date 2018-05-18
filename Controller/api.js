93ba407fa56d9b6c7b0bde8f5606e5bd8fe8467c
ShuangQiu <cainqs@outlook.com>
2018-05-18 16:00:21 +0800

    update

--- Controller/api.js
+++ Controller/api.js
@@ -40,34 +40,41 @@ app.get('/getmatched', function(req, res){
 });
 
 app.get('/video', function(req, res) {
-    const path = 'd:\\test.MP4'
-    const stat = fs.statSync(path)
-    const fileSize = stat.size
-    const range = req.headers.range
-    if (range) {
-      const parts = range.replace(/bytes=/, "").split("-")
-      const start = parseInt(parts[0], 10)
-      const end = parts[1] 
-        ? parseInt(parts[1], 10)
-        : fileSize-1
-      const chunksize = (end-start)+1
-      const file = fs.createReadStream(path, {start, end})
-      const head = {
-        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunksize,
-        'Content-Type': 'video/mp4',
-      }
-      res.writeHead(206, head);
-      file.pipe(res);
-    } else {
-      const head = {
-        'Content-Length': fileSize,
-        'Content-Type': 'video/mp4',
-      }
-      res.writeHead(200, head)
-      fs.createReadStream(path).pipe(res)
-    }
+    const path = scanService.getMatched(req.query)
+    .then(result =>{
+        const path = result[0].location
+        const stat = fs.statSync(path)
+        const fileSize = stat.size
+        const range = req.headers.range
+        if (range) {
+          const parts = range.replace(/bytes=/, "").split("-")
+          const start = parseInt(parts[0], 10)
+          const end = parts[1] 
+            ? parseInt(parts[1], 10)
+            : fileSize-1
+          const chunksize = (end-start)+1
+          const file = fs.createReadStream(path, {start, end})
+          const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+          }
+          res.writeHead(206, head);
+          file.pipe(res);
+        } else {
+          const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+          }
+          res.writeHead(200, head)
+          fs.createReadStream(path).pipe(res)
+        }
+
+    }).catch(err => {
+        console.log(err);
+    })
+
 });
 
 var server = app.listen(8888, function() {
